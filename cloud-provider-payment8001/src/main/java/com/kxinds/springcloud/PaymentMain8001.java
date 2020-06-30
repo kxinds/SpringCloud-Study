04a2955ecc24d03636d978b5eb03ae62d4887deb
kex <kex@yuanian.com>
2020-06-30 23:07:20 +0800

    新建子工程

---
+++ cloud-provider-payment8001/src/main/java/com/kxinds/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.kxinds.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*** @author:kexin
+ *   @company: 元年科技
+ *   @description:
+ *   @date: 2020/6/30
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
